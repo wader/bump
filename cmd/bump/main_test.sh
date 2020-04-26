03ac148bb3603a5eaab2160140d7741cccb8d676
Mattias Wadman <mattias.wadman@gmail.com>
2020-04-26 20:36:23 +0200

    Use shfmt

--- cmd/bump/main_test.sh
+++ cmd/bump/main_test.sh
@@ -8,15 +8,15 @@ function test_update() {
     local TESTDIR=$(mktemp -d)
     cd "$TESTDIR"
 
-    cat <<EOF > a
+    cat <<EOF >a
 bump: name /name: (\d+)/ static:456
 EOF
 
-    cat <<EOF > b
+    cat <<EOF >b
 name: 123
 EOF
 
-    "$BUMP" update a b > stdout 2> stderr 
+    "$BUMP" update a b >stdout 2>stderr
     local actual_exit_code=$?
     local actual_b=$(cat b)
     local actual_stdout=$(cat stdout)
@@ -26,19 +26,19 @@ EOF
     local expected_stdout=""
     local expected_stderr=""
 
-    if [ "$expected_exit_code" != "$actual_exit_code" ] ; then
+    if [ "$expected_exit_code" != "$actual_exit_code" ]; then
         echo "exit_code $expected_exit_code got $actual_exit_code"
         r=1
     fi
-    if [ "$expected_b" != "$actual_b" ] ; then
+    if [ "$expected_b" != "$actual_b" ]; then
         echo "expected_b $expected_b got $actual_b"
         r=1
     fi
-    if [ "$expected_stdout" != "$actual_stdout" ] ; then
+    if [ "$expected_stdout" != "$actual_stdout" ]; then
         echo "stdout $expected_stdout got $actual_stdout"
         r=1
     fi
-    if [ "$expected_stderr" != "$actual_stderr" ] ; then
+    if [ "$expected_stderr" != "$actual_stderr" ]; then
         echo "stderr $expected_stderr got $actual_stderr"
         r=1
     fi
@@ -53,15 +53,15 @@ function test_diff() {
     local TESTDIR=$(mktemp -d)
     cd "$TESTDIR"
 
-    cat <<EOF > a
+    cat <<EOF >a
 bump: name /name: (\d+)/ static:456
 EOF
 
-    cat <<EOF > b
+    cat <<EOF >b
 name: 123
 EOF
 
-    "$BUMP" diff a b > stdout 2> stderr 
+    "$BUMP" diff a b >stdout 2>stderr
     local actual_exit_code=$?
 
     cat stdout | patch -p0
@@ -73,15 +73,15 @@ EOF
     local expected_b="name: 456"
     local expected_stderr=""
 
-    if [ "$expected_exit_code" != "$actual_exit_code" ] ; then
+    if [ "$expected_exit_code" != "$actual_exit_code" ]; then
         echo "exit_code $expected_exit_code got $actual_exit_code"
         r=1
     fi
-    if [ "$expected_b" != "$actual_b" ] ; then
+    if [ "$expected_b" != "$actual_b" ]; then
         echo "expected_b $expected_b got $actual_b"
         r=1
     fi
-    if [ "$expected_stderr" != "$actual_stderr" ] ; then
+    if [ "$expected_stderr" != "$actual_stderr" ]; then
         echo "stderr $expected_stderr got $actual_stderr"
         r=1
     fi
@@ -91,17 +91,16 @@ EOF
     return $r
 }
 
-
 function test_error() {
     local r=0
     local TESTDIR=$(mktemp -d)
     cd "$TESTDIR"
 
-    cat <<EOF > a
+    cat <<EOF >a
 bump: name /name: (\d+)/ static:456
 EOF
 
-    "$BUMP" update a > stdout 2> stderr 
+    "$BUMP" update a >stdout 2>stderr
     local actual_exit_code=$?
     local actual_stdout=$(cat stdout)
     local actual_stderr=$(cat stderr)
@@ -109,15 +108,15 @@ EOF
     local expected_stdout=""
     local expected_stderr="a:1: name has no current version matches"
 
-    if [ "$expected_exit_code" != "$actual_exit_code" ] ; then
+    if [ "$expected_exit_code" != "$actual_exit_code" ]; then
         echo "exit_code $expected_exit_code got $actual_exit_code"
         r=1
     fi
-    if [ "$expected_stdout" != "$actual_stdout" ] ; then
+    if [ "$expected_stdout" != "$actual_stdout" ]; then
         echo "stdout $expected_stdout got $actual_stdout"
         r=1
     fi
-    if [ "$expected_stderr" != "$actual_stderr" ] ; then
+    if [ "$expected_stderr" != "$actual_stderr" ]; then
         echo "stderr $expected_stderr got $actual_stderr"
         r=1
     fi
