25cf284e5b7e52e4dda65e73444a9776156b8eef
Mattias Wadman <mattias.wadman@gmail.com>
2022-08-19 19:08:13 +0200

    docker: Switch to v2 API
    https://www.docker.com/blog/docker-hub-v1-api-deprecation/

--- internal/filter/docker/docker.go
+++ internal/filter/docker/docker.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"strings"
 
 	"github.com/wader/bump/internal/filter"
 )
@@ -20,11 +21,23 @@ Produce versions from a image on ducker hub.
 docker:alpine|^3
 `[1:]
 
-// TODO: other registry?
-const defaultIndex = `https://index.docker.io/v1/repositories/%s/tags`
+// TODO: support other registries
+// TODO: auth?
+const authURLTemplate = `https://auth.docker.io/token?service=registry.docker.io&scope=repository:%s:pull`
+const listTagsURLTemplate = `https://index.docker.io/v2/%s/tags/list`
 
-type tag struct {
-	Name string `json:"name"`
+// image -> library/image
+// repo/image -> repo/image
+func argToRepoImage(a string) (string, error) {
+	parts := strings.Split(a, "/")
+	switch {
+	case len(parts) == 0:
+		return "", fmt.Errorf("invalid name")
+	case len(parts) == 1:
+		return "library/" + parts[0], nil
+	default:
+		return a, nil
+	}
 }
 
 // New docker filter
@@ -36,19 +49,57 @@ func New(prefix string, arg string) (filter filter.Filter, err error) {
 		return nil, fmt.Errorf("needs a image name")
 	}
 
-	return dockerFilter{imageName: arg}, nil
+	repoImage, err := argToRepoImage(arg)
+	if err != nil {
+		return nil, fmt.Errorf("%w: %s", err, arg)
+	}
+
+	return dockerFilter{
+		imageName: arg,
+		repoImage: repoImage,
+	}, nil
 }
 
 type dockerFilter struct {
 	imageName string
+	repoImage string
 }
 
 func (f dockerFilter) String() string {
 	return Name + ":" + f.imageName
 }
 
+func (f dockerFilter) getToken() (string, error) {
+	r, err := http.Get(fmt.Sprintf(authURLTemplate, f.repoImage))
+	if err != nil {
+		return "", fmt.Errorf("token request failed: %w", err)
+	}
+	defer r.Body.Close()
+
+	var resp struct {
+		Token string `json:"token"`
+	}
+
+	if err := json.NewDecoder(r.Body).Decode(&resp); err != nil {
+		return "", fmt.Errorf("failed to parse token response: %w", err)
+	}
+
+	return resp.Token, nil
+}
+
 func (f dockerFilter) Filter(versions filter.Versions, versionKey string) (filter.Versions, string, error) {
-	r, err := http.Get(fmt.Sprintf(defaultIndex, f.imageName))
+	token, err := f.getToken()
+	if err != nil {
+		return nil, "", err
+	}
+
+	req, err := http.NewRequest(http.MethodGet, fmt.Sprintf(listTagsURLTemplate, f.repoImage), nil)
+	if err != nil {
+		return nil, "", err
+	}
+	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", token))
+
+	r, err := http.DefaultClient.Do(req)
 	if err != nil {
 		return nil, versionKey, err
 	}
@@ -58,15 +109,18 @@ func (f dockerFilter) Filter(versions filter.Versions, versionKey string) (filte
 		return nil, "", fmt.Errorf("error response: %s", r.Status)
 	}
 
-	var tags []tag
-	err = json.NewDecoder(r.Body).Decode(&tags)
+	var resp struct {
+		Tags []string `json:"tags"`
+	}
+
+	err = json.NewDecoder(r.Body).Decode(&resp)
 	if err != nil {
 		return nil, "", err
 	}
 
 	tagNames := append(filter.Versions{}, versions...)
-	for _, t := range tags {
-		tagNames = append(tagNames, filter.NewVersionWithName(t.Name, nil))
+	for _, t := range resp.Tags {
+		tagNames = append(tagNames, filter.NewVersionWithName(t, nil))
 	}
 
 	return tagNames, versionKey, nil
