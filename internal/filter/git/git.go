3bb05a1ab0e60729af6788738a446bee761828a0
Mattias Wadman <mattias.wadman@gmail.com>
2024-10-22 23:34:13 +0200

    Revert "git: Be bit more strict about tag versions"
    This reverts commit a6d2819d1fbb0acc97b64a07e1dbc7462f7bbcab.

--- internal/filter/git/git.go
+++ internal/filter/git/git.go
@@ -26,7 +26,7 @@ https://github.com/git/git.git|*
 
 // default ref filter
 // refs/tags/<non-digits><version-number> -> version-number
-var refFilterRe = regexp.MustCompile(`^refs/tags/[[:alpha:]]*([\d\.\-]+)$`)
+var refFilterRe = regexp.MustCompile(`^refs/tags/[^\d]*([\d\.\-]+)$`)
 
 // New git filter
 func New(prefix string, arg string) (filter filter.Filter, err error) {
