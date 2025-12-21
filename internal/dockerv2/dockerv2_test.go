bac7c9f80930217254519994c68e9fa3fbb1502f
Mattias Wadman <mattias.wadman@gmail.com>
2025-12-21 20:40:28 +0100

    dockerv2: Add pagination support
    Should fix issue with missing tags for docker filter

--- internal/dockerv2/dockerv2_test.go
+++ internal/dockerv2/dockerv2_test.go
@@ -18,3 +18,23 @@ func TestParseWWWAuth(t *testing.T) {
 		t.Fatalf("service %s", v)
 	}
 }
+
+func TestParseLinkHeader(t *testing.T) {
+	p, err := dockerv2.ParseLinkHeader(`</v2/library/debian/tags/list?last=oldoldstable-20210208&n=1000>; rel="next"`)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(p) != 1 {
+		t.Fatalf("len(p) != 1, is %d", len(p))
+	}
+	expectedRawURL := `/v2/library/debian/tags/list?last=oldoldstable-20210208&n=1000`
+	actualRawURL := p[0].RawURL
+	if expectedRawURL != actualRawURL {
+		t.Fatalf("expected RawURL %s, got %s", expectedRawURL, actualRawURL)
+	}
+	expectedParamsRel := "next"
+	actualParamsRel := p[0].Params["rel"]
+	if expectedParamsRel != actualParamsRel {
+		t.Fatalf("expected Params[rel] %s, got %s", expectedParamsRel, actualParamsRel)
+	}
+}
